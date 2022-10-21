7d11bbdc31f9f5fab86ae70cb6224bc17d1c38ae
Victor Toma <t_victor@icloud.com>
2022-10-21 21:10:37 +0300

    Drum Machine Design Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Pad from './components/Pad';
+import Switch from './components/Switch';
+import Volume from './components/Volume';
+import Display from './components/Display';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='root'>
+            <h1 className='main-title'>Drum Machine</h1>
+            <div className='drum-machine'>
+                <div className='drum-pads'>
+                    <Pad letter='Q' />
+                    <Pad letter='W' />
+                    <Pad letter='E' />
+                    <Pad letter='A' />
+                    <Pad letter='S' />
+                    <Pad letter='D' />
+                    <Pad letter='Z' />
+                    <Pad letter='X' />
+                    <Pad letter='C' />
+                </div>
+
+                <div className='control-panel'>
+                    <div className='switches'>
+                        <Switch
+                            switchId='switch1'
+                            toggleId='toggle1'
+                            label='BANK'
+                        />
+                        <Switch
+                            switchId='switch2'
+                            toggleId='toggle2'
+                            label='POWER'
+                        />
+                    </div>
+
+                    <Volume />
+
+                    <Display />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
